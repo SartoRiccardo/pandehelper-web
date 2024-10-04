1cda10baf1716a722ae54344e5976f63f8383d11
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-04 22:36:11 +0200

    feat: Generates metadata

--- src/app/setup/[module]/page.js
+++ src/app/setup/[module]/page.js
@@ -1,6 +1,15 @@
 import MarkdownPage from "@/components/MarkdownPage";
 import fs from "fs/promises";
 
+export async function generateMetadata({ params }) {
+  let title = params.module
+    .replace(/(^|-)(\w)/g, (_m, g1, g2) => `${g1}${g2.toUpperCase()}`)
+    .replace(/-/, " ");
+  return {
+    title: `${title} | PandeHelper`,
+  };
+}
+
 export default async function SetupGuide({ params }) {
   const contents = await fs.readFile(
     `${process.env.PAGES_FOLDER}/setup/${params.module}.md`,
