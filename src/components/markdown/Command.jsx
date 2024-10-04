31557244aecf7e7da71cad2c51ff1f6079e57e9b
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-04 02:08:43 +0200

    feat: Command styling and parsing

--- src/components/markdown/Command.jsx
+++ src/components/markdown/Command.jsx
@@ -1,3 +1,4 @@
+import cssCommand from "./Command.module.css";
 import Markdown from "react-markdown";
 
 export default function Command({ children }) {
@@ -6,12 +7,24 @@ export default function Command({ children }) {
   const paramList = (params?.split("\n>>> ") || []).slice(1);
 
   return (
-    <div className={cssGuide.command_info}>
-      <div>{command}</div>
-      <Markdown>{description}</Markdown>
-      {paramList.map((param, i) => (
-        <CommandParam key={i}>{param}</CommandParam>
-      ))}
+    <div className={cssCommand.command}>
+      <p
+        id={command.replace(/ /, "-").substring(1)}
+        className={cssCommand.name}
+      >
+        {command}
+      </p>
+      <div className={cssCommand.content}>
+        <Markdown>{description}</Markdown>
+        {paramList.length > 0 && (
+          <>
+            <p className={cssCommand.h_parameters}>Parameters</p>
+            {paramList.map((param, i) => (
+              <CommandParam key={i}>{param}</CommandParam>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 }
@@ -19,9 +32,13 @@ export default function Command({ children }) {
 function CommandParam({ children }) {
   const [paramInfo, ...description] = children.split("\n");
   const [param, ...properties] = paramInfo.split(/\s+/);
+  let cls = cssCommand.param;
+  if (properties.includes("required")) cls += " " + cssCommand.required;
   return (
     <div>
-      {param}
+      <span className={cls}>
+        <span>{param}</span>
+      </span>
       <Markdown>{description.join("\n")}</Markdown>
     </div>
   );
