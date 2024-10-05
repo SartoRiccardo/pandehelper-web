efd078536064f57643af6ce7f286e067d3a6c031
SartoRiccardo <rikki.sarto@gmail.com>
2024-10-05 11:33:50 +0200

    feat: Added service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+// https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started
+import withPWAInit from "@ducanh2912/next-pwa";
+
 const nextConfig = {
   output: "standalone",
   webpack(config) {
@@ -37,4 +40,163 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const runtimeCaching = [
+  {
+    urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
+    handler: "CacheFirst",
+    options: {
+      cacheName: "google-fonts-webfonts",
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+      },
+    },
+  },
+
+  {
+    urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "google-fonts-stylesheets",
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      },
+    },
+  },
+
+  {
+    urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "static-font-assets",
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      },
+    },
+  },
+
+  {
+    urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "static-image-assets",
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+
+  {
+    urlPattern: /\/_next\/image\?url=.+$/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "next-image",
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+
+  {
+    urlPattern: /\.(?:js)$/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "static-js-assets",
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+
+  {
+    urlPattern: /\.(?:css|less)$/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "static-style-assets",
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+
+  {
+    urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+    handler: "StaleWhileRevalidate",
+    options: {
+      cacheName: "next-data",
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+
+  {
+    urlPattern: /\.(?:json|xml|csv)$/i,
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "static-data-assets",
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      },
+    },
+  },
+
+  {
+    urlPattern: ({ url }) => {
+      const isSameOrigin = self.origin === url.origin;
+      const { searchParams, pathname } = url;
+      return (
+        !searchParams.get("_rsc") &&
+        isSameOrigin &&
+        !pathname.startsWith("/api/")
+      );
+    },
+    handler: "NetworkFirst",
+    options: {
+      cacheName: "others",
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60,
+      },
+      networkTimeoutSeconds: 10,
+    },
+  },
+
+  {
+    urlPattern: ({ url }) => {
+      const isSameOrigin = self.origin === url.origin;
+      const { searchParams, pathname } = url;
+      return (
+        searchParams.get("_rsc") &&
+        isSameOrigin &&
+        !pathname.startsWith("/api/")
+      );
+    },
+    handler: "CacheFirst",
+    options: {
+      cacheName: "others-prefetch",
+      expiration: {
+        maxEntries: 128,
+        maxAgeSeconds: 24 * 60 * 60,
+      },
+    },
+  },
+];
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  extendDefaultRuntimeCaching: false,
+  reloadOnOnline: false,
+  workboxOptions: { runtimeCaching },
+});
+
+export default withPWA(nextConfig);
